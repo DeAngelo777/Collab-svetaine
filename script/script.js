6b7743c04bed99dc96476730a88b1912fdea45bc
Laimonas <ldirgelas530@gmail.com>
2024-08-11 16:30:58 +0300

    up

--- script/script.js
+++ script/script.js
@@ -24,14 +24,3 @@ function nextTestimonial() {
 showTestimonial(currentIndex);
 
 
-// Sticky Navbar
-
-const navbar = document.getElementById('navbar');
-
-window.onscroll = function () {
-    if(window.pageYOffset > 100) {
-        navbar.classList.remove('headerContainer');
-    } else {
-        navbar.classList.add('headerContainer');
-    }
-}
